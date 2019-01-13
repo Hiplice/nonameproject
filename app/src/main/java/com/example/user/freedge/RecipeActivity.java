3be0e891bca0774ed1faff39c1e250b232724863
Evgeniy <j.borodin1@mail.ru>
2019-01-13 14:31:06 +0300

    Title

--- app/src/main/java/com/example/user/freedge/RecipeActivity.java
+++ app/src/main/java/com/example/user/freedge/RecipeActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -16,8 +15,7 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         // Плавающая кнопка снизу
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
