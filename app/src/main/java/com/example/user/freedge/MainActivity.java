a5d7cf551a79c8239d67fc1178c91f2638e24c1c
Evgeniy <j.borodin1@mail.ru>
2019-01-14 14:39:16 +0300

    fragments

--- app/src/main/java/com/example/user/freedge/MainActivity.java
+++ app/src/main/java/com/example/user/freedge/MainActivity.java
@@ -1,152 +1,33 @@
 package com.example.user.freedge;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements ProductMenuListView.ItemClickListener {
+import com.example.user.freedge.Fragments.Products;
+import com.example.user.freedge.Fragments.Recipes;
 
-    FrameLayout container;
-    ProductMenuListView adapter;
-    FragmentManager myFragmentManager;
-    MyFragment1 myFragment1;
-    MyFragment2 myFragment2;
-    final static String TAG_1 = "FRAGMENT_1";
-    final static String TAG_2 = "FRAGMENT_2";
-    final static String KEY_MSG_1 = "FRAGMENT1_MSG";
-    final static String KEY_MSG_2 = "FRAGMENT2_MSG";
 
-    // класс для первого фрагмента
-    public static class MyFragment1 extends Fragment {
+public class MainActivity extends AppCompatActivity {
 
-        TextView textMsg;
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.first_screen, null);
-            textMsg = (TextView) view.findViewById(R.id.f);
-
-            Bundle bundle = getArguments();
-            if (bundle != null) {
-                String msg = bundle.getString(KEY_MSG_1);
-                if (msg != null) {
-                    textMsg.setText(msg);
-                }
-            }
-            return view;
-        }
-
-        public void setMsg(String msg) {
-            textMsg.setText(msg);
-        }
-    }
-
-    // класс для второго фрагмента
-    public static class MyFragment2 extends Fragment {
-
-        TextView textMsg;
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.recipe, null);
-            textMsg = (TextView) view.findViewById(R.id.q);
-
-            Bundle bundle = getArguments();
-            if (bundle != null) {
-                String msg = bundle.getString(KEY_MSG_2);
-                if (msg != null) {
-                    textMsg.setText(msg);
-                }
-            }
-            return view;
-        }
-
-        public void setMsg(String msg) {
-            textMsg.setText(msg);
-        }
-    }
+    android.app.FragmentTransaction transaction;
+    Recipes recipes;
+    Products products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        products = new Products();
+        recipes = new Recipes();
 
-        container = (FrameLayout) findViewById(R.id.container);
+        transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, products);
+        transaction.commit();
 
-
-        ImageButton button1 = findViewById(R.id.productMenuButton);
-        ImageButton button2 = findViewById(R.id.recipeMenuButton);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                MyFragment1 fragment = (MyFragment1) myFragmentManager
-                        .findFragmentByTag(TAG_1);
-
-                if (fragment == null) {
-
-                    Bundle bundle = new Bundle();
-                    bundle.putString(KEY_MSG_1, "Заменили на первый фрагмент");
-                    myFragment1.setArguments(bundle);
-
-                    FragmentTransaction fragmentTransaction = myFragmentManager
-                            .beginTransaction();
-                    fragmentTransaction.replace(R.id.container, myFragment1,
-                            TAG_1);
-                    fragmentTransaction.commit();
-
-                } else {
-                    fragment.setMsg("Первый фрагмент уже загружен");
-                }
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                MyFragment2 fragment = (MyFragment2) myFragmentManager
-                        .findFragmentByTag(TAG_2);
-
-                if (fragment == null) {
-
-                    Bundle bundle = new Bundle();
-                    bundle.putString(KEY_MSG_2, "Заменили на второй фрагмент");
-                    myFragment2.setArguments(bundle);
-
-                    FragmentTransaction fragmentTransaction = myFragmentManager
-                            .beginTransaction();
-                    fragmentTransaction.replace(R.id.container, myFragment2,
-                            TAG_2);
-                    fragmentTransaction.commit();
-
-                } else {
-                    fragment.setMsg("Второй фрагмент уже загружен");
-                }
-            }
-        });
 
         //Третья кнопка панели управления
         ImageButton button3 = findViewById(R.id.settingMenuButton);
@@ -160,14 +41,18 @@ public class MainActivity extends AppCompatActivity implements ProductMenuListVi
                 startActivity(intent3);
             }
         });
+
     }
 
-
-
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    public void onProducts(View view) {
+        transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, products);
+        transaction.commit();
     }
 
-
+    public void onRecipes(View view) {
+        transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, recipes);
+        transaction.commit();
+    }
 }
