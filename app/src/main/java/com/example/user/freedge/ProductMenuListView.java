8f953100061f7f1a6cf528385175383c1e67633c
Hiplice <tetant0n@gmail.com>
2019-01-14 16:52:57 +0300

    сделал смену иконок ура c:

--- app/src/main/java/com/example/user/freedge/ProductMenuListView.java
+++ app/src/main/java/com/example/user/freedge/ProductMenuListView.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ public class ProductMenuListView extends RecyclerView.Adapter<ProductMenuListVie
 
     private ArrayList<ArrayList<String>> mData;
     private ArrayList<Integer> mColors;
+    private ArrayList<Integer> mIcons;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
     private Context context;
@@ -26,6 +28,7 @@ public class ProductMenuListView extends RecyclerView.Adapter<ProductMenuListVie
         this.mData = data;
         this.context = context;
         mColors = DataHandler.getCategoryColorsById(mData.get(4));
+        mIcons = DataHandler.getCategoryIconsById(mData.get(4));
     }
 
     // inflates the row layout from xml when needed
@@ -42,6 +45,7 @@ public class ProductMenuListView extends RecyclerView.Adapter<ProductMenuListVie
         holder.weightText.setText(mData.get(2).get(position));
         holder.productAddDate.setText( mData.get(3).get(position));
         holder.menuElement.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, mColors.get(position))));
+        holder.categoryImage.setImageResource(mIcons.get(position));
     }
 
     // total number of rows
@@ -57,9 +61,11 @@ public class ProductMenuListView extends RecyclerView.Adapter<ProductMenuListVie
         TextView boldProductName;
         TextView productAddDate;
         TextView weightText;
+        ImageView categoryImage;
 
         ViewHolder(View itemView) {
             super(itemView);
+            categoryImage = itemView.findViewById(R.id.categoryImage);
             boldProductName = itemView.findViewById(R.id.boldProductName);
             productAddDate = itemView.findViewById(R.id.productAddDate);
             weightText = itemView.findViewById(R.id.weightText);
