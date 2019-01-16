f3f7373954449b1c9c5e1a851bf0679ec26ceb20
Hiplice <tetant0n@gmail.com>
2019-01-16 15:52:35 +0300

    removed db, changed JDK 1_8

--- app/src/main/java/com/example/user/freedge/DataBaseHelper.java
+++ app/src/main/java/com/example/user/freedge/DataBaseHelper.java
@@ -1,17 +1,29 @@
 package com.example.user.freedge;
 
 import android.content.Context;
-import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DataBaseHelper extends SQLiteAssetHelper {
+public class DataBaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "fridge.db";
     private static final int DATABASE_VERSION = 1;
     private Context context;
 
     public DataBaseHelper(Context context) {
+
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 }
