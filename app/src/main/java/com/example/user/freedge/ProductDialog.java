ab9092df79d41685e8ef990a5266104f17cc1210
Evgeniy <j.borodin1@mail.ru>
2019-01-23 08:58:07 +0300

    do nothing

--- app/src/main/java/com/example/user/freedge/ProductDialog.java
+++ app/src/main/java/com/example/user/freedge/ProductDialog.java
@@ -2,13 +2,17 @@ package com.example.user.freedge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProductDialog extends AppCompatActivity {
 
     AutoCompleteTextView mAutoCompleteTextView;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,17 @@ public class ProductDialog extends AppCompatActivity {
         mAutoCompleteTextView = findViewById(R.id.product);
         mAutoCompleteTextView.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line, mData[1]));
+        name = mAutoCompleteTextView.getText().toString();
+    }
 
+    public void onOk(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),
+                name, Toast.LENGTH_SHORT);
+        toast.show();
+        //DataHandler.addProduct(name);
+    }
+
+    public void onCancel(View view) {
+        finish();
     }
 }
